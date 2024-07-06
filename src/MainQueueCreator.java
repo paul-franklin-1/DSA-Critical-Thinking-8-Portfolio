9474515f1553a3cd92393fafab63bf46c25ed2e2
Paul Franklin <paul.franklin@csuglobal.edu>
2024-07-06 12:59:04 -0400

    created classes, constructor, and first method

--- src/MainQueueCreator.java
+++ src/MainQueueCreator.java
@@ -2,4 +2,4 @@ public class MainQueueCreator {
     public static void main (String[] Args){
 
 
-}
+}}
--- src/PersonQueueManager.java
+++ src/PersonQueueManager.java
@@ -1,2 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
 public class PersonQueueManager {
+    private String lastName;
+    private String firstName;
+    private int age;
+    private Queue<PersonQueueManager> personQueue;
+    public PersonQueueManager(String lastName, String firstName, int age){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.age = age;
+    }
+    public void addToQueue(PersonQueueManager person){
+        personQueue.add(person);
+        System.out.println("Person \"" + person + "\" has been added to the queue.");
+    }
+
 }
